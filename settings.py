3686e56d2c944434585a6a60c0681f7070706e25
sheway <67420772+sheway@users.noreply.github.com>
2021-06-19 16:53:15 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,18 @@
-URL = "商品連結"
+product_id = "商品ID" #填入 https://24h.pchome.com.tw/prod/ 後的商品ID
 CHROME_PATH=r"--user-data-dir=C:\\<chrome 設定檔路徑>"  # 可透過網址列輸入 chrome://version/ 找到
 
 # 請注意！以下皆為機密個資，請小心謹慎，勿上傳至公開平台
-ACC = "PChome 登入帳號"
-PWD = "PChome 登入密碼"
+loginAcc = "PChome 登入帳號"
+loginPwd = "PChome 登入密碼"
+BuyerName = "購買者姓名"
+BuyerGender = "購買者性別(男/女)"
 BuyerSSN = "身分證字號"
-BirthYear, BirthMonth, BirthDay = "出生年", "月", "日"
-multi_CVV2Num = "信用卡檢查碼（背面三碼）"
+BirthYear, BirthMonth, BirthDay = "出生年(民國)", "月", "日"
+CardNum_single = "信用卡卡號"
+multi_ThruMonth = "信用卡有效月"
+multi_ThruYear = "信用卡有效年"
+multi_CVV2Num = "信用卡末三碼"
+BuyerMobile = "購買者手機"
+BuyerAddrCity = "信用卡帳單地址(縣市)"
+BuyerAddrRegion = "信用卡帳單地址(區)"
+BuyerAddr = "信用卡帳單地址"
