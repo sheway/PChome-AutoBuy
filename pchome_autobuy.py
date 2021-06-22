8daa7ae98d9886c5456a8bfa93de5b34d6005dab
sheway <67420772+sheway@users.noreply.github.com>
2021-06-22 12:15:31 +0800

    Update pchome_autobuy.py

--- pchome_autobuy.py
+++ pchome_autobuy.py
@@ -9,38 +9,38 @@ product_id = ''
 
 def login_acc():
     driver.get("https://ecvip.pchome.com.tw/login/v3/login.htm?")  #前往登入頁面
-    WebDriverWait(driver, 20).until(
+    WebDriverWait(driver, 20).until( #等待帳號輸入框加載完畢
         expected_conditions.presence_of_element_located((By.ID, 'loginAcc'))
     )
-    elem = driver.find_element_by_id('loginAcc')
+    elem = driver.find_element_by_id('loginAcc') #尋找帳號輸入框
     elem.clear()
-    elem.send_keys(loginAcc)
-    elem = driver.find_element_by_id('loginPwd')
+    elem.send_keys(loginAcc) #輸入帳號
+    elem = driver.find_element_by_id('loginPwd') #尋找密碼輸入框
     elem.clear()
-    elem.send_keys(loginPwd)
-    WebDriverWait(driver, 20).until(
+    elem.send_keys(loginPwd) #輸入密碼
+    WebDriverWait(driver, 20).until( #等待登入按鈕變成可按下
         expected_conditions.element_to_be_clickable((By.ID, "btnLogin"))
     )
-    driver.find_element_by_id('btnLogin').click()
+    driver.find_element_by_id('btnLogin').click() #按下登入按鈕
 
 def run_script():
     print("放入購物車")
-    WebDriverWait(driver, 20).until(
+    WebDriverWait(driver, 20).until( #等待放入購物車按鈕加載完畢
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//li[@id='ButtonContainer']/button"))
     )
-    driver.find_element_by_xpath("//li[@id='ButtonContainer']/button").click()
+    driver.find_element_by_xpath("//li[@id='ButtonContainer']/button").click() #按下放入購物車按鈕
 
     print("前往購物車")
     driver.get("https://ecssl.pchome.com.tw/sys/cflow/fsindex/BigCar/BIGCAR/ItemList")  #直接前往購物車
 
     #前往結帳 (一次付清) ### (要使用 JS 的方式 execute_script 點擊)
     print("前往結帳")
-    WebDriverWait(driver, 20).until(
+    WebDriverWait(driver, 20).until( #等待'一次付清'按鈕的 class 加載完畢
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//li[@class='CC']/a[@class='ui-btn']"))
     )
-    button = driver.find_element_by_xpath(
+    button = driver.find_element_by_xpath( #按下'一次付清'按鈕
         "//li[@class='CC']/a[@class='ui-btn']")
     driver.execute_script("arguments[0].click();", button)
 
@@ -48,7 +48,7 @@ def run_script():
     #BuyerName, BuyerGender, BuyerSSN, BirthYear, BirthMonth, BirthDay, CardNum_single, multi_ThruMonth, multi_ThruYear, multi_CVV2Num, BuyerMobile, BuyerAddrCity, BuyerAddrRegion, BuyerAddr
     print("填入各項資料")
 
-    # BuyerName
+    #填入買家姓名
     WebDriverWait(driver, 10).until(
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//input[@id='BuyerName']"))
@@ -57,12 +57,13 @@ def run_script():
     elem.clear()
     elem.send_keys(BuyerName)
 
-    # BuyerGender
+    #填入買家性別
     if BuyerGender == "男":
         driver.find_element_by_xpath("//input[@value='M']").click()
     else:
         driver.find_element_by_xpath("//input[@value='F']").click()
 
+    # 填入買家身分證
     WebDriverWait(driver, 20).until(
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//input[@id='BuyerSSN']"))
@@ -71,6 +72,7 @@ def run_script():
     elem.clear()
     elem.send_keys(BuyerSSN)
 
+    # 填入買家生日年月日
     WebDriverWait(driver, 20).until(
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//input[@name='BirthYear']"))
@@ -95,7 +97,7 @@ def run_script():
     elem.clear()
     elem.send_keys(BirthDay)
 
-    # CardNum_single
+    #填入買家信用卡卡號
     WebDriverWait(driver, 20).until(
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//input[@name='CardNum_single']"))
@@ -104,17 +106,17 @@ def run_script():
     elem.clear()
     elem.send_keys(CardNum_single)
 
-    # multi_ThruMonth
+    #填入買家信用卡到期月
     parent = driver.find_element_by_id("multi_ThruMonth")
     path = './/option[@value="' + multi_ThruMonth + '"]'
     parent.find_element_by_xpath(path).click()
 
-    # multi_ThruYear
+    #填入買家信用卡到期年
     parent = driver.find_element_by_id("multi_ThruYear")
     path = './/option[@value="' + multi_ThruYear + '"]'
     parent.find_element_by_xpath(path).click()
 
-    # multi_CVV2Num
+    #填入買家信用卡末三碼
     WebDriverWait(driver, 20).until(
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//input[@name='multi_CVV2Num']"))
@@ -123,7 +125,7 @@ def run_script():
     elem.clear()
     elem.send_keys(multi_CVV2Num)
 
-    # BuyerMobile
+    #填入買家手機
     WebDriverWait(driver, 20).until(
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//input[@name='BuyerMobile']"))
@@ -132,19 +134,19 @@ def run_script():
     elem.clear()
     elem.send_keys(BuyerMobile)
 
-    # BuyerAddrCity
+    #填入買家信用卡帳單地址(縣市)
     parent = driver.find_element_by_id("BuyerAddrCity")
     city_array = ["基隆市", "臺北市", "新北市", "桃園市", "新竹市", "新竹縣", "苗栗縣", "臺中市", "彰化縣", "南投縣", "雲林縣", "嘉義市", "嘉義縣", "臺南市", "高雄市", "屏東縣", "臺東縣", "花蓮縣", "宜蘭縣", "澎湖縣", "金門縣", "連江縣"]
     path = './/option[@value="' + str(city_array.index(BuyerAddrCity)) + '"]'
     parent.find_element_by_xpath(path).click()
 
-    # BuyerAddrRegion
+    #填入買家信用卡帳單地址(區)
     parent = driver.find_element_by_id("BuyerAddrRegion")
     path = './/option[@data-region="' + BuyerAddrRegion + '"]'
     # print("BuyerAddrRegion", path)
     parent.find_element_by_xpath(path).click()
 
-    # BuyerAddr
+    #填入買家信用卡帳單地址(剩餘地址)
     WebDriverWait(driver, 20).until(
         expected_conditions.element_to_be_clickable(
             (By.XPATH, "//input[@name='BuyerAddr']"))
@@ -175,13 +177,13 @@ def run_script():
     )
     driver.find_element_by_xpath("//input[@name='chk_agree']").click()
 
-    # ### 送出訂單 ### (要使用 JS 的方式 execute_script 點擊)
-    # WebDriverWait(driver, 20).until(
-    #     expected_conditions.element_to_be_clickable(
-    #         (By.XPATH, "//a[@id='btnSubmit']"))
-    # )
-    # button = driver.find_element_by_xpath("//a[@id='btnSubmit']")
-    # driver.execute_script("arguments[0].click();", button)
+    ### 送出訂單 ### (要使用 JS 的方式 execute_script 點擊)
+    WebDriverWait(driver, 20).until(
+        expected_conditions.element_to_be_clickable(
+            (By.XPATH, "//a[@id='btnSubmit']"))
+    )
+    button = driver.find_element_by_xpath("//a[@id='btnSubmit']")
+    driver.execute_script("arguments[0].click();", button)
 
 def get_products_sale_status():
     url = "https://ecapi.pchome.com.tw/ecshop/prodapi/v2/prod/button&id=" + product_id
@@ -215,7 +217,7 @@ if __name__ == "__main__":
     driver.get(url)
 
     #登入帳戶
-    # login_acc()
+    login_acc()
 
     flag = True
     number = 0
